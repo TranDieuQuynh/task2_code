8afb0b454a673a81a835440e849b38e226682107
tdquynh <trandieuquynh274@gmail.com>
2025-06-16 17:52:34 +0700

    feat: up

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_URL = process.env.REACT_APP_API_URL || '/api';
+export const API_URL = `${process.env.REACT_APP_API_URL}/api`;
+export const BACKEND_BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
